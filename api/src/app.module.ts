f221f35dad14f87fffaf39ee0620527b91ff513c
Stuart Kaija <stuartkaija@gmail.com>
2023-11-14 12:01:39 -0800

    update: added serve static module to api

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,8 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client')
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })

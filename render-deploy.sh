eb75771c559bde0360aa50b90067071c7d429db3
Stuart Kaija <stuartkaija@gmail.com>
2024-01-09 09:52:34 -0800

    update deploy script

--- render-deploy.sh
+++ render-deploy.sh
@@ -2,15 +2,14 @@
 # exit on error
 set -o errexit
 
+cd ./api/
+npm install
+npm run build
+
+cd ../client/
 npm install next@^13.4.19
+npm install
+npm run build
 
-# build API
-npm install --prefix api
-npm run build --prefix api
-
-# build client
-npm install --prefix client
-npm run build --prefix client
-
-# copy client to api
-cp -r .client/out ./api/client
+cd ../
+cp -r ./client/out ./api/client
